a53f69b99ab0344bcb18aed549dc159914828cfe
tapakorn.ode <tapakorn.ode@gmail.com>
2025-08-29 10:07:50 +0700

    how to use ts file show on page html

--- angular01/src/app/app.ts
+++ angular01/src/app/app.ts
@@ -8,5 +8,13 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular01');
+  title = "how are u";
+  name = "tapakorn";
+
+  hello(){
+    let x =10
+    const y =20
+    var data = 30
+    return x+y
+  }
 }
